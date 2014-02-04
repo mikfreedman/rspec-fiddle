0557ae91529af7f9af3b77ab67a252051a9d515a
Michael Freedman <github@michael-freedman.com>
2014-02-04 18:29:32 -0500

    POC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thing::Application.config.secret_token = '9d6e28baf8fa7f87061a01b5c433d6f551462c43ff6e97623deed88bbdeba3fdd1e929a8bb4796803d9724048e204c110e04d5d40716445029cde64068caeed2'
